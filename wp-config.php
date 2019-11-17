6e4791d3a277bb12a54e17ff18a1d01cb4aa7da3
jossouvil <43849653+jossouvil@users.noreply.github.com>
2019-11-17 20:47:02 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,10 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_HOME','https://pgpidb2.herokuapp.com');
+
+define('WP_SITEURL','https://pgpidb2.herokuapp.com');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
